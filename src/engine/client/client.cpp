1f36fb91fedb444ac9326278de92215af1712061
TsFreddie <whatis@tsdo.in>
2025-01-06 17:22:53 +0800

    unicode name support

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -1017,7 +1017,9 @@ const char *CClient::DummyName()
 	}
 	if(pBase)
 	{
-		str_format(m_aAutomaticDummyName, sizeof(m_aAutomaticDummyName), "[D] %s", pBase);
+		char aBuf[MAX_NAME_ARRAY_SIZE + 4];
+		str_format(aBuf, sizeof(aBuf), "[D] %s", pBase);
+		str_utf8_copy_num(m_aAutomaticDummyName, aBuf, MAX_NAME_LENGTH);
 		return m_aAutomaticDummyName;
 	}
 	return "brainless tee";
@@ -4376,6 +4378,46 @@ void CClient::ConchainStdoutOutputLevel(IConsole::IResult *pResult, void *pUserD
 	}
 }
 
+void CClient::ConchainPlayerName(IConsole::IResult *pResult, void *pUserData, IConsole::FCommandCallback pfnCallback, void *pCallbackUserData)
+{
+	pfnCallback(pResult, pCallbackUserData);
+
+	// truncate after the string config variable is set
+	char aTruncated[MAX_NAME_ARRAY_SIZE];
+	str_utf8_copy_num(aTruncated, g_Config.m_PlayerName, MAX_NAME_LENGTH);
+	mem_copy(g_Config.m_PlayerName, aTruncated, sizeof(g_Config.m_PlayerName));
+}
+
+void CClient::ConchainPlayerClan(IConsole::IResult *pResult, void *pUserData, IConsole::FCommandCallback pfnCallback, void *pCallbackUserData)
+{
+	pfnCallback(pResult, pCallbackUserData);
+
+	// truncate after the string config variable is set
+	char aTruncated[MAX_CLAN_ARRAY_SIZE];
+	str_utf8_copy_num(aTruncated, g_Config.m_PlayerClan, MAX_CLAN_LENGTH);
+	mem_copy(g_Config.m_PlayerClan, aTruncated, sizeof(g_Config.m_PlayerClan));
+}
+
+void CClient::ConchainDummyName(IConsole::IResult *pResult, void *pUserData, IConsole::FCommandCallback pfnCallback, void *pCallbackUserData)
+{
+	pfnCallback(pResult, pCallbackUserData);
+
+	// truncate after the string config variable is set
+	char aTruncated[MAX_NAME_ARRAY_SIZE];
+	str_utf8_copy_num(aTruncated, g_Config.m_ClDummyName, MAX_NAME_LENGTH);
+	mem_copy(g_Config.m_ClDummyName, aTruncated, sizeof(g_Config.m_ClDummyName));
+}
+
+void CClient::ConchainDummyClan(IConsole::IResult *pResult, void *pUserData, IConsole::FCommandCallback pfnCallback, void *pCallbackUserData)
+{
+	pfnCallback(pResult, pCallbackUserData);
+
+	// truncate after the string config variable is set
+	char aTruncated[MAX_CLAN_ARRAY_SIZE];
+	str_utf8_copy_num(aTruncated, g_Config.m_ClDummyClan, MAX_CLAN_LENGTH);
+	mem_copy(g_Config.m_ClDummyClan, aTruncated, sizeof(g_Config.m_ClDummyClan));
+}
+
 void CClient::RegisterCommands()
 {
 	m_pConsole = Kernel()->RequestInterface<IConsole>();
@@ -4454,6 +4496,11 @@ void CClient::RegisterCommands()
 
 	m_pConsole->Chain("loglevel", ConchainLoglevel, this);
 	m_pConsole->Chain("stdout_output_level", ConchainStdoutOutputLevel, this);
+
+	m_pConsole->Chain("player_name", ConchainPlayerName, this);
+	m_pConsole->Chain("player_clan", ConchainPlayerClan, this);
+	m_pConsole->Chain("dummy_name", ConchainDummyName, this);
+	m_pConsole->Chain("dummy_clan", ConchainDummyClan, this);
 }
 
 static CClient *CreateClient()
--- src/engine/shared/config_variables.h
+++ src/engine/shared/config_variables.h
@@ -212,8 +212,8 @@ MACRO_CONFIG_INT(UiUnreadNews, ui_unread_news, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG
 MACRO_CONFIG_INT(GfxNoclip, gfx_noclip, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Disable clipping")
 
 // dummy
-MACRO_CONFIG_STR(ClDummyName, dummy_name, 16, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Name of the dummy")
-MACRO_CONFIG_STR(ClDummyClan, dummy_clan, 12, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Clan of the dummy")
+MACRO_CONFIG_STR(ClDummyName, dummy_name, MAX_NAME_ARRAY_SIZE, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Name of the dummy")
+MACRO_CONFIG_STR(ClDummyClan, dummy_clan, MAX_CLAN_ARRAY_SIZE, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Clan of the dummy")
 MACRO_CONFIG_INT(ClDummyCountry, dummy_country, -1, -1, 1000, CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Country of the Dummy")
 MACRO_CONFIG_INT(ClDummyUseCustomColor, dummy_use_custom_color, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE | CFGFLAG_INSENSITIVE, "Toggles usage of custom colors")
 MACRO_CONFIG_COL(ClDummyColorBody, dummy_color_body, 65408, CFGFLAG_CLIENT | CFGFLAG_SAVE | CFGFLAG_COLLIGHT | CFGFLAG_INSENSITIVE, "Dummy body color")
@@ -288,8 +288,8 @@ MACRO_CONFIG_INT(DbgDummies, dbg_dummies, 0, 0, MAX_CLIENTS, CFGFLAG_SERVER, "Ad
 
 MACRO_CONFIG_INT(DbgTuning, dbg_tuning, 0, 0, 2, CFGFLAG_CLIENT, "Display information about the tuning parameters that affect the own player (0 = off, 1 = show changed, 2 = show all)")
 
-MACRO_CONFIG_STR(PlayerName, player_name, 16, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Name of the player")
-MACRO_CONFIG_STR(PlayerClan, player_clan, 12, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Clan of the player")
+MACRO_CONFIG_STR(PlayerName, player_name, MAX_NAME_ARRAY_SIZE, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Name of the player")
+MACRO_CONFIG_STR(PlayerClan, player_clan, MAX_CLAN_ARRAY_SIZE, "", CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Clan of the player")
 MACRO_CONFIG_INT(PlayerCountry, player_country, -1, -1, 1000, CFGFLAG_SAVE | CFGFLAG_CLIENT | CFGFLAG_INSENSITIVE, "Country of the player")
 
 MACRO_CONFIG_STR(Password, password, 256, "", CFGFLAG_CLIENT | CFGFLAG_SERVER | CFGFLAG_NONTEEHISTORIC, "Password to the server")
